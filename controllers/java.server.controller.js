5ff3c63e022801357cda40bf5e866e4a7f0aedba
januNithi <csejananim@gmail.com>
2016-08-23 10:52:03 +0530

    Updating java integration

--- controllers/java.server.controller.js
+++ controllers/java.server.controller.js
@@ -2,12 +2,11 @@
  * Created by CSS on 22-08-2016.
  */
 
-var java = require("java");
-
-var System = java.import('java.lang.System');
+var javaInit = require("javaInit.server.controller.js");
+var java = javaInit.getJavaInstance();
 
 exports.getImages = function (req,res) {
 
-    System.out.println("Inside Java coding");
-
+    var Main = java.Main();
+    console.log(Main);
 };
---
+++ controllers/javaInit.server.controller.js
@@ -0,0 +1,17 @@
+/**
+ * Created by CSS on 23-08-2016.
+ */
+var fs = require("fs");
+var java = require("java");
+var baseDir = "./javaLib";
+var dependencies = fs.readdirSync(baseDir);
+
+dependencies.forEach(function(dependency){
+    java.classpath.push(baseDir + "/" + dependency);
+})
+
+java.classpath.push("./classes");
+
+exports.getJavaInstance = function() {
+    return java;
+}
