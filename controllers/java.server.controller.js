fa6a5e02fdf44be7b3c4321c6710fcd84fb590ea
januNithi <csejananim@gmail.com>
2016-08-23 11:00:20 +0530

    Updating java integration

--- controllers/java.server.controller.js
+++ controllers/java.server.controller.js
@@ -2,7 +2,7 @@
  * Created by CSS on 22-08-2016.
  */
 
-var javaInit = require("javaInit.server.controller.js");
+var javaInit = require("./javaInit.server.controller.js");
 var java = javaInit.getJavaInstance();
 
 exports.getImages = function (req,res) {
