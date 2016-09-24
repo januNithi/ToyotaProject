ff8f96d8a888288314ad1dc9fa5a701196397ca6
januNithi <csejananim@gmail.com>
2016-09-24 12:32:01 +0530

    commit db file

--- config/sqlDb.js
+++ config/sqlDb.js
@@ -6,13 +6,29 @@ module.exports = {
     user: 'sa',
     password: 'sa',
     server: 'localhost',
-    database: 'toyota_service'
+    database: 'toyota_service',
+    connectionTimeout: 60000,
+    idleTimeoutMillis: 60000,
+    requestTimeout: 60000,
+    pool: {
+        max: 10,
+        min: 0,
+        timeout:60000
+    }
 };
-
+//
 //
 // module.exports = {
 //     user: 'student',
 //     password: 'jv9N#w21',
 //     server: 'cyberstudents.in',
-//     database: 'student'
+//     database: 'student',
+//     connectionTimeout: 60000,
+//     idleTimeoutMillis: 60000,
+//     requestTimeout: 60000,
+//     pool: {
+//         max: 10,
+//         min: 0,
+//         timeout:120000
+//     }
 // };
