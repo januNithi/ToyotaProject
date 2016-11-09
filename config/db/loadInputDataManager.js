1504cbdc1535850d9eecac99f6b12a0a7f5ec0d3
januNithi <csejananim@gmail.com>
2016-11-09 18:58:41 +0530

    Updated with background images.

--- config/db/loadInputDataManager.js
+++ config/db/loadInputDataManager.js
@@ -2115,7 +2115,7 @@ function getRegisteredService(){
     var connection = new sql.Connection(sqlDb);
 
     connection.connect().then(function () {
-        var query = "Select Sid,Mid,MType,SDate as date,DATEDIFF(mi,SDate,EDate) as time from Toyota_FINRegister where Status = 'Completed' order by id in ASC";
+        var query = "Select Sid,Mid,MType,SDate as date,DATEDIFF(mi,SDate,EDate) as time from Toyota_FINRegister where Status = 'Completed' ORDER BY YEAR(SDate) DESC, MONTH(SDate) DESC, DAY(SDATE) DESC";
         var request = new sql.Request(connection);
 
         console.log(query);
